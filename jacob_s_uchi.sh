102bf09ee618c2f1ba67a7dc9c9caaf16745f2b4
JacobHA <jacobhadamczyk@gmail.com>
2023-11-22 18:04:56 -0500

    pushes for my (jacob) sweep bash

--- jacob_s_uchi.sh
+++ jacob_s_uchi.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=u-chi
 #SBATCH --time=3-23:00:00
 #SBATCH --mem-per-cpu=12gb
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=3
 
 # Set filenames for stdout and stderr.  %j can be used for the jobid.
 # see "filename patterns" section of the sbatch man page for
@@ -14,7 +14,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=DGXA100
 #SBATCH --export=NONE
-#SBATCH --array=1-3
+##SBATCH --array=1-3
 echo "using scavenger"
 
 # Prepare conda:
@@ -24,15 +24,10 @@ export CPATH=$CPATH:$CONDA_PREFIX/include
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib
 export MUJOCO_GL="glfw"
 
+# # Set the Weights and Biases environment variables
+# export WANDB_MODE=offline
+# wandb offline
 
-echo "Start Run"
-echo `date`
-# python uchi_agent_NN.py
-python experiments/wandb_job.py
-python experiments/atari_sweep.yml
-# python darer/MultiLogU.py
-# python local_finetuned_runs.py -a logu
-
-# Diagnostic/Logging Information
-echo "Finish Run"
-echo "end time is `date`"
+# Start the evaluations
+EXPNAME=${1:-"atari-mini"}
+python experiments/atari_sweep.py --local-wandb True --proj u-chi-learning --exp-name $EXPNAME
