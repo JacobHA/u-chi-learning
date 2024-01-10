d4533124b0cabc67caf920f392162e094a25a2a2
JacobHA <jacobhadamczyk@gmail.com>
2024-01-10 13:58:55 -0500

    dtype test

--- tests/test_AtariWrappers.py
+++ tests/test_AtariWrappers.py
@@ -1,4 +1,3 @@
-import pytest
 import sys
 sys.path.append('../darer')
 sys.path.append('darer')
@@ -11,3 +10,4 @@ def test_atari_env_id_to_envs():
     env, eval_env = atari_env_id_to_envs(env_id, render=False, n_envs=1, frameskip=4, framestack_k=4)
     obs, _ = env.reset()
     assert obs.shape == (84, 84, 4)
+    assert obs.dtype == 'uint8'
