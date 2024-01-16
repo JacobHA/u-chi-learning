was accidentally sampling from buffer twice, cat->stack, functional torch, fixed polyak, fixed eigvec calc, tuning maze hparams, softmax w/ temp for rawlik, new kl calc., fire reset env for breakout

darer/BaseAgent.py
@@ -76,7 +76,7 @@ class BaseAgent:
                  max_grad_norm: float = 10,
                  learning_starts=5_000,
                  aggregator: str = 'max',
-                 loss_fn: torch.nn.modules.loss = torch.nn.MSELoss(),#torch.nn.HuberLoss(),
+                 loss_fn: torch.nn.modules.loss = torch.nn.functional.mse_loss,#torch.nn.functional.HuberLoss(),
                  device: Union[torch.device, str] = "auto",
                  render: bool = False,
                  tensorboard_log: Optional[str] = None,
@@ -143,6 +143,8 @@ class BaseAgent:
         self.beta_end = beta_end
         self.scheduler_str = scheduler_str
         self.train_this_step = False
+        # Track the rewards over time:
+        self.step_to_avg_eval_rwd = {}
 
         self.replay_buffer = ReplayBuffer(buffer_size=buffer_size,
                                           observation_space=self.env.observation_space,
@@ -162,7 +164,7 @@ class BaseAgent:
         self._n_updates = 0
         self.env_steps = 0
         # self._initialize_networks()
-        self.loss_fn = F.smooth_l1_loss if loss_fn is None else loss_fn
+        self.loss_fn = loss_fn
 
     def log_hparams(self, logger):
         # Log the hparams:
@@ -202,7 +204,6 @@ class BaseAgent:
 
             loss = self.gradient_descent(batch, grad_step)
 
-            self.logger.record("train/loss", loss.item())
             self.optimizers.zero_grad()
 
             # Clip gradient norm
@@ -212,6 +213,7 @@ class BaseAgent:
 
         # TODO: Clamp based on reward range
         # new_thetas = torch.clamp(new_thetas, self.min_rwd, self.max_rwd)
+        self.new_thetas = torch.clamp(self.new_thetas, min=-50, max=50)
         # Log both theta values:
         for idx, new_theta in enumerate(self.new_thetas.T):
             self.logger.record(f"train/theta_{idx}", new_theta.mean().item())
@@ -369,7 +371,13 @@ class BaseAgent:
 
         if self.is_tabular:
             # Record the error in the eigenvector:
-            fa_eigvec = get_eigvec_values(self).flatten()
+            if self.algo_name == 'LogU':
+                log=True
+            elif self.algo_name =='U':
+                log=False
+            else:
+                raise ValueError(f"Unknown agent name: {self.name}. Use U/LogU (defaults).")
+            fa_eigvec = get_eigvec_values(self, logu=log).flatten()
             err = np.abs(self.true_eigvec - fa_eigvec).max()
             self.logger.record('train/eigvec_err', err.item())
 
@@ -421,6 +429,7 @@ class BaseAgent:
         self.eval_time = eval_time
         self.eval_fps = eval_fps
         self.avg_eval_rwd = avg_reward
+        self.step_to_avg_eval_rwd[self.env_steps] = avg_reward
         return avg_reward
 
     def _beta_scheduler(self, beta_schedule, total_timesteps):

darer/Models.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.distributions import Categorical
 import numpy as np
-from stable_baselines3.common.utils import zip_strict
+from stable_baselines3.common.utils import polyak_update, zip_strict
 from gymnasium import spaces
 import gymnasium as gym
 from torch.optim.lr_scheduler import StepLR, MultiplicativeLR, LinearLR, ExponentialLR, LRScheduler
@@ -233,8 +233,10 @@ class TargetNets():
         with torch.no_grad():
             # zip does not raise an exception if length of parameters does not match.
             for new_params, target_params in zip(online_nets.parameters(), self.parameters()):
-                for new_param, target_param in zip_strict(new_params, target_params):
-                    target_param.data.mul_(tau).add_(new_param.data, alpha=1.0-tau)
+                # for new_param, target_param in zip_strict(new_params, target_params):
+                #     target_param.data.mul_(tau).add_(new_param.data, alpha=1.0-tau)
+                #TODO: 
+                polyak_update(new_params, target_params, tau)
 
     def parameters(self):
         """
@@ -434,7 +436,7 @@ class Usa(nn.Module):
         x = self.fc2(x)
         x = self.relu(x)
         x = self.fc3(x)
-        return nn.Softplus()(x)
+        return nn.functional.softplus(x)
 
 
 # Initialize Policy weights
@@ -527,7 +529,7 @@ class UNet(nn.Module):
                                   activation,
                                   hidden_dim,
                                   self.device)
-        weights_init_(model)
+        # weights_init_(model)
 
         # Add a softplus layer:
         model = nn.Sequential(model, nn.Softplus())#beta=0.2))
@@ -578,6 +580,8 @@ class UNet(nn.Module):
         # get machine epsilon:
         eps = torch.finfo(torch.float32).eps
         assert (y >= 0).all(), f"u must be non-negative. u={y}"
+        # Clamp above eps:
+        y = torch.clamp(y, min=eps)
         return y
         # return x
         
@@ -606,13 +610,13 @@ class PiNet(nn.Module):
                                   self.device)
         # weights_init_(model)
         # initialize the net to have zero bias and identical weights:
-        for m in model:
-            if isinstance(m, nn.Linear):
-                nn.init.uniform_(m.weight, a=0, b=1/10)
-                nn.init.constant_(m.bias, 0)
+        # for m in model:
+        #     if isinstance(m, nn.Linear):
+        #         nn.init.uniform_(m.weight, a=0, b=1/10)
+        #         nn.init.constant_(m.bias, 0)
 
         # Add a softplus layer:
-        model = nn.Sequential(model, nn.Softmax(dim=-1))
+        # model = nn.Sequential(model, nn.Softmax(dim=-1))
         model.to(self.device)
         self.model = model
         self.nS = nS
@@ -656,7 +660,10 @@ class PiNet(nn.Module):
                     if (x.shape == self.nS).all():
                         x = x.unsqueeze(0)
                                 
-        y = self.model(x)
+        y = self.model(x) / 10
+        y = torch.nn.functional.softmax(y, dim=-1)
+        
+        
         # get machine epsilon:
         eps = torch.finfo(torch.float32).eps
         assert (y >= 0).all(), f"u must be non-negative. u={y}"

darer/UAgent.py
@@ -13,8 +13,8 @@ class UAgent(BaseAgent):
     def __init__(self,
                  *args,
                  use_rawlik=False,
-                 prior_update_interval: int = 1_000,
-                 prior_tau: float = 0.1,
+                 prior_update_interval: int = 5_000,
+                 prior_tau: float = 0.25,
                  name: Optional[str] = None,
                  **kwargs,
                  ):
@@ -56,7 +56,7 @@ class UAgent(BaseAgent):
             self.target_prior.load_state_dicts(
                 [u.state_dict() for u in self.online_prior])
             opts.append(torch.optim.Adam(
-                self.online_prior.parameters(), lr=self.learning_rate))
+                self.online_prior.nets[0].parameters(), lr=self.learning_rate))
 
         self.optimizers = Optimizers(opts, self.scheduler_str)
 
@@ -66,14 +66,15 @@ class UAgent(BaseAgent):
             # return self.env.action_space.sample()
             if self.use_rawlik:
                 pi0 = self.target_prior.nets[0](state).squeeze()
-                pi0 /= pi0.sum()
+                # pi0 /= pi0.sum()
             else:
-                pi0 = 1 / self.nA
+                pi0 = torch.ones(self.nA, device=self.device) * (1/self.nA)
             chosen_action = self.online_us.choose_action(state, prior=pi0, greedy=False)
             pi_by_pi0 = self.aggregator_fn(torch.stack(
-                [u(state) for u in self.online_us], dim=0), 
-                dim=0)
-            kl = torch.log(pi_by_pi0[0][chosen_action])
+                [u(state) for u in self.online_us], dim=0), dim=0)
+            # kl = pi_by_pi0[0][chosen_action] * pi0[chosen_action] * torch.log(pi_by_pi0[0][chosen_action])
+            kl = (pi_by_pi0[0] * pi0* torch.log(pi_by_pi0[0])).sum()
+
             kl = float(kl.item())
             # chosen_action = self.env.action_space.sample()
             return chosen_action, kl
@@ -82,7 +83,7 @@ class UAgent(BaseAgent):
         with torch.no_grad():
             if self.use_rawlik:
                 pi0 = self.target_prior.nets[0](state).squeeze()
-                pi0 /= pi0.sum()
+                # pi0 /= pi0.sum()
             else:
                 pi0 = None
 
@@ -94,30 +95,28 @@ class UAgent(BaseAgent):
 
     def _on_step(self):
         super()._on_step()
+        #TODO: put this in _update_target
         if self.use_rawlik:
             if self.env_steps % self.prior_update_interval == 0:
                 self.target_prior.polyak(self.online_prior, self.prior_tau)
 
     def gradient_descent(self, batch, grad_step: int):
-        # Sample a batch from the replay buffer:
-        batch = self.replay_buffer.sample(self.batch_size)
         states, actions, next_states, dones, rewards = batch
         # rewards[dones.bool()] -= 1
         # Calculate the current u values (feedforward):
-        curr_u = torch.cat([online_u(states).squeeze().gather(1, actions.long())
+        # curr_ua = torch.stack([online_u(states).squeeze()
+        #                     for online_u in self.online_us], dim=1)
+        curr_u = torch.stack([online_u(states).squeeze().gather(1, actions.long())
                             for online_u in self.online_us], dim=1)
         if self.use_rawlik:
-            curr_priora = self.online_prior.nets[0](
-                states).squeeze()
-            # normalize the prior:
-            # curr_priora /= curr_priora.sum(dim=-1, keepdim=True)
-
+            curr_priora = self.online_prior.nets[0](states).squeeze()
 
         with torch.no_grad():
             online_u_next = torch.stack([u(next_states)
                                          for u in self.online_us], dim=0)
             online_curr_u = torch.stack([u(states).gather(1, actions)
                                          for u in self.online_us], dim=0)
+            #TODO: collapse these into one call
             online_curr_ua = torch.stack([u(states)
                                           for u in self.online_us], dim=0)
             if self.use_rawlik:
@@ -138,11 +137,12 @@ class UAgent(BaseAgent):
                 online_u_next * target_prior_next.repeat(self.num_nets, 1, 1)).sum(dim=-1)
             online_curr_u = online_curr_u.squeeze(-1)
             in_log = online_chi / online_curr_u
+            # in_log = torch.clamp(in_log, min=1e-6, max=1e6)
             # clamp to a tolerable range:
             batch_theta = -torch.mean(rewards.squeeze(-1) + torch.log(in_log) / self.beta, dim=1)
+            # batch_theta = torch.clamp(batch_theta, min=-30, max=30)
             self.new_thetas[grad_step, :] = batch_theta
             
-
             target_next_us = [target_u(next_states) for target_u in self.target_us]
 
             # logsumexp over actions:
@@ -151,17 +151,21 @@ class UAgent(BaseAgent):
 
             next_chis = (target_next_u * target_prior_next).sum(dim=-1)
 
-            next_chis = next_chis.unsqueeze(-1)
+            next_chis = next_chis.reshape(-1,1)
             assert next_chis.shape == dones.shape
             next_chis = next_chis * (1-dones)  # + 1 * dones
 
             # "Backup" eigenvector equation:
             in_exp = rewards + self.theta
+            in_exp = torch.clamp(in_exp, max=30)
             expected_curr_u = torch.exp(self.beta * (in_exp)) * next_chis
-            expected_curr_u = expected_curr_u.squeeze(1)
+            expected_curr_u = expected_curr_u
+
+        # self.logger.record("train/u-avg", torch.mean(curr_u, dim=0))
 
         # Calculate the u ("critic") loss:
-        loss = 0.5*sum(self.loss_fn(u, expected_curr_u) for u in curr_u.T)
+        loss = 0.5*sum(self.loss_fn(u, expected_curr_u) for u in curr_u.permute(1, 0, 2))
+        self.logger.record("train/loss", loss.item())
         if self.use_rawlik:
             # prior_loss = self.loss_fn(curr_prior.squeeze(), self.aggregator_fn(online_curr_u,dim=0)[0])
             pistar = self.aggregator_fn(online_curr_ua, dim=0) * target_priora
@@ -172,10 +176,14 @@ class UAgent(BaseAgent):
 
             self.logger.record("train/prior_loss", prior_loss.item())
             # prior_loss.backward()
+            loss += prior_loss
 
-            return loss, prior_loss
-
-        return loss
+        # weight = 1e-1
+        # Mean across actions:
+        # curr_umean = torch.mean(curr_ua, dim=-1, keepdim=True).repeat(1, 1, self.nA)
+        # regularize_loss = weight * torch.nn.functional.l1_loss(curr_ua, curr_umean)
+        # self.logger.record("train/regularize_loss", regularize_loss.item())
+        return loss #+ regularize_loss
 
 
 def main():
@@ -189,16 +197,17 @@ def main():
     # env_id = 'Acrobot-v1'
     # env_id = 'LunarLander-v2'
     # env_id = 'PongNoFrameskip-v4'
+    env_id = 'BreakoutNoFrameskip-v4'
     # env_id = 'FrozenLake-v1'
     # env_id = 'MountainCar-v0'
     # env_id = 'Drug-v0'
 
-    from hparams import cartpole_u2 as config
+    from hparams import nature_pong as config
 
-    agent = UAgent(env_id, **config, device='cpu', log_interval=1000,
+    agent = UAgent(env_id, **config, device='cuda', log_interval=5000,
                    tensorboard_log='pong', num_nets=2, render=False, #aggregator='min',
-                   beta_schedule='none', use_rawlik=True,
-                   loss_fn=torch.nn.MSELoss(), beta_end=5)
+                   beta_schedule='none', use_rawlik=False,
+                   beta_end=5)
     #    scheduler_str='none')  # , beta_schedule='none', beta_end=2.4,
     # use_rawlik=True)
     # Measure the time it takes to learn:

tabular_vs_FA.py
@@ -14,9 +14,10 @@ from tabular.frozen_lake_env import ModifiedFrozenLake, MAPS
 
 config = maze
 config.pop('beta')
-map_name = '5x12ridge'
-def exact_solution(beta, env):
+map_name = '3x5uturn'
+# map_name = 'hallway1'
 
+def exact_solution(beta, env):
     dynamics, rewards = get_dynamics_and_rewards(env.unwrapped)
     n_states, SA = dynamics.shape
     n_actions = int(SA / n_states)
@@ -34,10 +35,10 @@ def exact_solution(beta, env):
 def FA_solution(beta, env):
     # Use an agent to solve the environment
 
-    agent = UAgent(env, **config, log_interval=1000, tensorboard_log='pend',
-                        num_nets=2, device='cuda',# use_rawlik=False,
-                        beta=beta, render=False, aggregator='max')
-    agent.learn(total_timesteps=300_000)
+    agent = UAgent(env, **config, log_interval=1000, tensorboard_log='pong',
+                        num_nets=2, device='cpu',# use_rawlik=False,
+                        beta=beta, render=False)#, aggregator='min')
+    agent.learn(total_timesteps=50_000)
     get_eigvec_values(agent, save_name=f'tabular/tabular_expt/data/{map_name}eigvec')
     # convert agent.theta to float
     theta = agent.theta.item()
@@ -45,7 +46,7 @@ def FA_solution(beta, env):
 
 def main():
     # initialize the environment
-    n_action = 5
+    n_action = 4
     max_steps = 200
     desc = np.array(MAPS[map_name], dtype='c')
     env_src = ModifiedFrozenLake(
@@ -61,7 +62,7 @@ def main():
     betas = np.logspace(1, 1, 4)
     betas = [14,19,24,30,37]
     betas = np.linspace(1, 50, 50)
-    # betas = [3.]
+    betas = [10.]
 
     exact = [exact_solution(beta, env) for beta in betas]
     print(exact)
@@ -104,8 +105,10 @@ def plot():
 
     # Plot the eigenvectors:
     true_eigvec = np.load(f'tabular/tabular_expt/data/{map_name}u_true.npy')
-    fa_logeigvec = np.load(f'tabular/tabular_expt/data/{map_name}eigvec.npy')
-    fa_u = np.exp(fa_logeigvec.flatten())
+    # fa_logeigvec = np.load(f'tabular/tabular_expt/data/{map_name}eigvec.npy')
+    fa_eigvec = np.load(f'tabular/tabular_expt/data/{map_name}eigvec.npy')
+    fa_u = fa_eigvec.flatten()
+    # fa_u = np.exp(fa_logeigvec.flatten())
     true_u = true_eigvec.flatten()
     # normalize them to have the same norm:
     true_u /= np.linalg.norm(true_u)

tests/test_TargetNets.py
@@ -11,7 +11,7 @@ def target_nets():
     return TargetNets(list_of_nets)
 
 @pytest.fixture
-def target_mixture_nets():
+def online_nets():
     # Create a list of nets for polyak testing
     num_nets = 3
     list_of_nets = [torch.nn.Linear(10, 5) for _ in range(num_nets)]
@@ -38,41 +38,49 @@ def test_load_state_dict(target_nets):
 
 import copy
 
-def test_polyak(target_nets, target_mixture_nets):
+def test_polyak(target_nets, online_nets):
     # Create some dummy parameters
 
     # Perform polyak update
-    tau = 0.5
+    tau = 0.25
     with torch.no_grad():
         # deep copy the original nets:
         original_nets = copy.deepcopy(target_nets)
-        target_nets.polyak(target_mixture_nets, tau)
+        # SB3 Polyak update:
+        target_nets.polyak(online_nets, tau)
 
         # Check if the nets have been updated
-        for updated_net, mixture_net, original_net in zip(target_nets, target_mixture_nets, original_nets):
+        for updated_net, mixture_net, original_net in zip(target_nets, online_nets, original_nets):
             for updated_param, mix_param, original_param in zip(updated_net.parameters(), mixture_net.parameters(), original_net.parameters()):
                 # Ensure each parameter is updated correctly
-                mix_param.data.mul_(1 - tau)
-                original_param.data.mul_(tau)
+                mix_param.data.mul_(tau)
+                original_param.data.mul_(1 - tau)
                 result = torch.add(mix_param.data, original_param.data)
                 assert torch.allclose(updated_param.data, result)
 
-def test_zero_polyak(target_nets, target_mixture_nets):
+def test_zero_polyak(target_nets, online_nets):
     # Perform polyak update
-    tau = 1.0
+    tau = 0.0
     with torch.no_grad():
         # deep copy the original nets:
         original_nets = copy.deepcopy(target_nets)
-        target_nets.polyak(target_mixture_nets, tau)
+        target_nets.polyak(online_nets, tau)
 
-        # Check if the nets have been updated
-        for updated_net, mixture_net, original_net in zip(target_nets, target_mixture_nets, original_nets):
-            for updated_param, mix_param, original_param in zip(updated_net.parameters(), mixture_net.parameters(), original_net.parameters()):
-                # Ensure each parameter is updated correctly
-                mix_param.data.mul_(1 - tau)
-                original_param.data.mul_(tau)
-                result = torch.add(mix_param.data, original_param.data)
-                assert torch.allclose(original_param.data, result)
+        # Check the target nets params haven't changed:
+        for original_target, updated_target in zip(original_nets, target_nets):
+            for original_param, updated_param in zip(original_target.parameters(), updated_target.parameters()):
+                assert torch.allclose(original_param, updated_param)
+        
+def test_one_polyak(target_nets, online_nets):
+    # Perform polyak update
+    tau = 1.0
+    with torch.no_grad():
+        target_nets.polyak(online_nets, tau)
+
+        # Check the target nets params haven't changed:
+        for online_net, updated_target in zip(online_nets, target_nets):
+            for online_param, updated_param in zip(online_net.parameters(), updated_target.parameters()):
+                assert torch.equal(online_param, updated_param)
 
 
 def test_parameters(target_nets):