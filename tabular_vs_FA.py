5c25e46c7d1250309af3a063779098b6427c66b2
JacobHA <jacobhadamczyk@gmail.com>
2024-01-18 15:14:30 -0500

    proper saving, longer training, factor out betas

--- tabular_vs_FA.py
+++ tabular_vs_FA.py
@@ -29,7 +29,7 @@ def exact_solution(beta, env):
 
     print(f"l_true: {l_true}")
     # save u true:
-    np.save(f'tabular/tabular_expt/data/{map_name}u_true.npy', u_true)
+    np.save(f'tabular/tabular_expt/data/{map_name}_{beta}u_true.npy', u_true)
     return -np.log(l_true) / beta
 
 def FA_solution(beta, env):
@@ -38,13 +38,13 @@ def FA_solution(beta, env):
     agent = UAgent(env, **config, log_interval=1000, tensorboard_log='pong',
                         num_nets=2, device='cpu',# use_rawlik=False,
                         beta=beta, render=False)#, aggregator='min')
-    agent.learn(total_timesteps=50_000)
-    get_eigvec_values(agent, save_name=f'tabular/tabular_expt/data/{map_name}eigvec')
+    agent.learn(total_timesteps=250_000)
+    get_eigvec_values(agent, save_name=f'tabular/tabular_expt/data/{map_name}_{beta}eigvec')
     # convert agent.theta to float
     theta = agent.theta.item()
     return theta
 
-def main():
+def main(beta):
     # initialize the environment
     n_action = 4
     max_steps = 200
@@ -58,26 +58,41 @@ def main():
     )
     env = TimeLimit(env_src, max_episode_steps=max_steps)
 
-    # Set the beta values to test
-    betas = np.logspace(1, 1, 4)
-    betas = [14,19,24,30,37]
-    betas = np.linspace(1, 50, 50)
-    betas = [10.]
-
-    exact = [exact_solution(beta, env) for beta in betas]
+    exact = exact_solution(beta, env)#for beta in betas]
     print(exact)
-    FA = [FA_solution(beta, env) for beta in betas]
+    FA = FA_solution(beta, env) #for beta in betas
 
-    # save the data:
-    data = pd.DataFrame({'beta': betas, 'exact': exact, 'FA': FA})
+    # save the single beta data point with index (all scalar values):
+    data = pd.DataFrame({'beta': beta, 'exact': exact, 'FA': FA}, index=[0])
     # get number of files with same prefix:
     num = len([f for f in os.listdir('tabular/tabular_expt/data') if f.startswith(f'{map_name}tabular_vs_FA')])
-    data.to_csv(f'tabular/tabular_expt/data/{map_name}tabular_vs_FA_{num}.csv', index=False)
+    data.to_csv(f'tabular/tabular_expt/data/{map_name}_{beta}_tabular_vs_FA_{num}.csv', index=False)
+
+    plot_eigvec(beta)
+
+
+def plot_eigvec(beta):
+    # Plot the eigenvectors:
+    true_eigvec = np.load(f'tabular/tabular_expt/data/{map_name}_{beta}u_true.npy')
+    # fa_logeigvec = np.load(f'tabular/tabular_expt/data/{map_name}eigvec.npy')
+    fa_eigvec = np.load(f'tabular/tabular_expt/data/{map_name}_{beta}eigvec.npy')
+    fa_u = fa_eigvec.flatten()
+    # fa_u = np.exp(fa_logeigvec.flatten())
+    true_u = true_eigvec.flatten()
+    # normalize them to have the same norm:
+    true_u /= np.linalg.norm(true_u)
+    fa_u /= np.linalg.norm(fa_u)
+
     plt.figure()
-    plt.plot(betas, exact, 'ko-', label='Exact')
-    plt.plot(betas, FA, 'bo', label='FA')
+    plt.plot(true_u, 'ko-', label='Exact')
+    plt.plot(fa_u, 'ro', label='FA')
+    plt.xlabel('State-action pairs')
+    plt.ylabel('Eigenvector')
+    plt.title(f'Eigenvectors on {map_name}')
     plt.legend()
-    plt.savefig(f'tabular/tabular_expt/results/{map_name}tabular_vs_FA.png')
+    plt.savefig(f'tabular/tabular_expt/results/{map_name}_{beta}_eigvec.png')
+
+
 
 def plot():
     # Plot the data in all CSV files
@@ -103,26 +118,20 @@ def plot():
     plt.legend()
     plt.savefig(f'tabular/tabular_expt/results/{map_name}tabular_vs_FA.png')
 
-    # Plot the eigenvectors:
-    true_eigvec = np.load(f'tabular/tabular_expt/data/{map_name}u_true.npy')
-    # fa_logeigvec = np.load(f'tabular/tabular_expt/data/{map_name}eigvec.npy')
-    fa_eigvec = np.load(f'tabular/tabular_expt/data/{map_name}eigvec.npy')
-    fa_u = fa_eigvec.flatten()
-    # fa_u = np.exp(fa_logeigvec.flatten())
-    true_u = true_eigvec.flatten()
-    # normalize them to have the same norm:
-    true_u /= np.linalg.norm(true_u)
-    fa_u /= np.linalg.norm(fa_u)
-    plt.figure()
-    plt.plot(true_u, 'ko-', label='Exact')
-    plt.plot(fa_u, 'ro', label='FA')
-    plt.xlabel('State-action pairs')
-    plt.ylabel('Eigenvector')
-    plt.title(f'Eigenvectors on {map_name}')
-    plt.legend()
-    plt.savefig(f'tabular/tabular_expt/results/{map_name}eigvec.png')
+
 
 
 if __name__ == '__main__':
-    main()
+    import multiprocessing as mp
+
+    # pool = mp.Pool(15)
+    #looks like levels off around beta=100, beta=0.1
+    alphas = np.linspace(1/50, 10, 10)[::-1]
+    betas = 1 / alphas
+    # pool.map(main, betas)
+    # pool.close()
+    # pool.join()
+    for beta in betas:
+        main(beta)
+    
     plot()
