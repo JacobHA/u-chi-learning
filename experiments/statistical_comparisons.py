correct scaling and labels for rliable

experiments/statistical_comparisons.py
@@ -8,12 +8,33 @@ import os
 from glob import glob
 import pandas as pd
 from tbparse import SummaryReader
+import argparse
 
+env_to_steps = {
+    'CartPole-v1': 50_000,
+    'Acrobot-v1': 100_000,
+    'LunarLander-v2': 200_000,
+    'MountainCar-v0': 200_000,
+    'HalfCheetah-v4': 1_000_000,
+    'Ant-v4': 1_000_000,
+    'Swimmer-v4': 250_000,
+    'Humanoid-v4': 5_000_000,
+    'Pusher-v4': 1_000_000,
+    'Pendulum-v1': 10_000,
+}
 plot_metrics = ['eval/avg_reward']
-env = 'HalfCheetah-v4'
+
+# parse env arg:
+args = argparse.ArgumentParser()
+args.add_argument('--env', type=str, default='MountainCar-v0')
+args = args.parse_args()
+
+env = args.env
 # env='Ant-v4'
+total_steps = env_to_steps[env]
 algorithms = [f'{env}-ASAC', 'SAC0.990.2', 'SAC', f'{env}-arDDPG']
 algorithms += [f'{env}-arSAC-newh', 'SAC0.990.2', 'SAC', f'{env}-arDDPG']
+algorithms += ['DQN', f'{env}-ASQL']
 algorithms = list(set(algorithms))
 
 folder = f'ft_logs/EVAL/{env}/'
@@ -50,7 +71,13 @@ for subfolder in os.listdir(folder):
                 # Add run number:
                 df['run'] = os.path.basename(subfolder).split('_')[1]
                 # algo_data = pd.concat([algo_data, df])
-                score_dict[algo_name].append(sum(df['value'].tolist()) * df['step'].values[0] / 1e6)
+                last_steps = df['step'].values[-1]
+                log_freq = df['step'].values[1] - df['step'].values[0]
+                # This (roughly) ensures the run is complete:
+                if not last_steps in [total_steps, total_steps-log_freq+1]:
+                    print(f"Warning: {algo_name} has {last_steps} steps. Skipping...")
+                    continue
+                score_dict[algo_name].append(sum(df['value'].tolist()) * log_freq / total_steps)
                 # Get rewards at intervals of 5000:
                 # reward_sequence = np.array(df['value'][df['step'] % 5000 == 0].tolist())
                 # if len(reward_sequence) != 200:
@@ -75,6 +102,10 @@ algorithms = list(score_dict.keys())
 for algo in algorithms:
     score_dict[algo] = np.array(score_dict[algo]).reshape(-1, 1)
 
+# Sort the score_dict by alphabetical name of algo to ensure plotting color consistency:
+score_dict = dict(sorted(score_dict.items(), key=lambda x: x[0]))
+
+
 aggregate_func = lambda x: np.array([
   metrics.aggregate_median(x),
   metrics.aggregate_iqm(x),
@@ -87,12 +118,22 @@ fig, axes = plot_utils.plot_interval_estimates(
   aggregate_scores, aggregate_score_cis,
   metric_names=['Median', 'IQM', 'Mean'],
   algorithms=algorithms, xlabel='Average Evaluation Reward',
-  xlabel_y_coordinate=-1)
+  xlabel_y_coordinate=-1,
+  legend=True)
 
 fig.savefig(f'ft_logs/EVAL/{env}/rliabl.png', bbox_inches='tight')
+min_t = np.min([np.min(scores) for scores in aggregate_scores.values()])
+max_t = np.max([np.max(scores) for scores in aggregate_scores.values()])
+if min_t > 0:
+    min_t *= 0.75
+else:
+    min_t *= 1.25
 
-max_t = 1.25 * np.max([np.max(scores) for scores in aggregate_scores.values()])
-thresholds = np.linspace(0.0, max_t, 81)
+if max_t > 0:
+    max_t *= 1.25
+else:
+    max_t *= 0.75
+thresholds = np.linspace(min_t, max_t, 81)
 score_distributions, score_distributions_cis = rly.create_performance_profile(
     score_dict, thresholds, reps=2000)
 # Plot score distributions
@@ -101,8 +142,9 @@ plot_utils.plot_performance_profiles(
   score_distributions, thresholds,
   performance_profile_cis=score_distributions_cis,
   colors=dict(zip(algorithms, sns.color_palette('colorblind'))),
-  xlabel=r'Human Normalized Score $(\tau)$',
-  ax=ax)
+  xlabel=r'Average Evaluation Reward $(\tau)$',
+  ax=ax,
+  legend=True)
 
 fig.savefig(f'ft_logs/EVAL/{env}/perf_profile.png', bbox_inches='tight')
 