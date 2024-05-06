5db98c3e87d4aa8d4d51bf843ec7e7cfa99e687e
JacobHA <jacobhadamczyk@gmail.com>
2024-05-06 10:01:01 -0400

    clean up, prep for pendulum

--- darer/arDDPG.py
+++ darer/arDDPG.py
@@ -273,23 +273,17 @@ class Agent:
 
 config = {
     'seed': 0,
-    'lr_actor': 3e-4,
-    'lr_critic': 3e-4,
-    'lr_rho': 3e-4,
+    'lr_actor': 2e-3,
+    'lr_critic': 2e-3,
+    'lr_rho': 2e-3,
     'actor_hidden': 256,
     'critic_hidden': 256,
-    'tau': 0.005,
+    'tau': 0.995,
     'buffer_size': 100_000,
     'batch_size': 256,
-    'episodes': 1000,
-    'warmup_samples': 10000,
     'critic_freq': 1,
     'actor_freq': 2,
-    'epi_len': 1000,
-    'epi_len_eval': 1000,
-    'total_env_steps': 1_000_000,
-    'eval_freq': 1000,
-    'update_freq': 1,
+    'eval_freq': 200,
     'device': 'cuda',
     'log': True,
 }    
@@ -297,25 +291,12 @@ config = {
 
 config['seed'] = 10
 config['buffer_size'] = int(1e5)
-config['total_env_steps'] = int(1e6)
 config['batch_size'] = 256
-config['warmup_samples'] = 5000
-config['eval_freq'] = 5000
-config['epi_len'] = 1000
-config['epi_len_eval'] = 10000
 config['act_fn'] = nn.ReLU
-config['tau'] = 0.995
 config['log'] = True
-config['update_freq'] = 1000
 config['critic_freq'] = 1
 config['actor_freq'] = 2
 
-
-config['lr_critic'] = 3e-4
-config['lr_actor'] = 3e-4
-config['lr_rho'] = 3e-4
-config['critic_hidden'] = 128
-config['actor_hidden'] = 128
 
 torch.manual_seed(config['seed'])
 np.random.seed(config['seed'])
@@ -323,7 +304,7 @@ np.random.seed(config['seed'])
 class arDDPG(BaseAgent):
     def __init__(self, *args, **kwargs):
 
-        super().__init__(*args, **kwargs)
+        super().__init__(*args, learning_starts=0, **kwargs)
         self.algo_name = 'arDDPG'
         self.tensorboard_log = 'ft_logs/EVAL/' + args[0]
         self.logger = logger_at_folder(self.tensorboard_log,
@@ -361,22 +342,10 @@ class arDDPG(BaseAgent):
         # Target update is done in the update method of the Agent class.
         pass
 
-# env = dmc2gym.make(config['domain'],config['task'], episode_length = config['epi_len'])
-# env_eval = dmc2gym.make(config['domain'],config['task'], episode_length = config['epi_len_eval'])
-
-# buffer = Buffer(config['buffer_size'])
-# agent = Agent(env_eval, config)
-# total_env_steps = config['total_env_steps']
-# batch_size = config['batch_size']
-# learning_start = config['warmup_samples']
-# eval_freq = config['eval_freq']
-# update_freq = config['update_freq']
-
-# epi_len = config['epi_len']
 
 def main():
-    agent = arDDPG('HalfCheetah-v4', 3e-4, 1)
-    agent.learn(total_timesteps=1_000_000)
+    agent = arDDPG('Pendulum-v1', 3e-4, 1)
+    agent.learn(total_timesteps=10_000)
 
 if __name__ == '__main__':
     for _ in range(10):
