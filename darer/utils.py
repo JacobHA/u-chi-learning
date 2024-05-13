1d7ce35ae868bdeecc8f35eab3004d00f2c25e8d
JacobHA <jacobhadamczyk@gmail.com>
2024-05-13 13:43:12 -0400

    fix the uniform sampler - wasn't saving to sampled dict unless q_unif. added assert

--- darer/utils.py
+++ darer/utils.py
@@ -233,7 +233,8 @@ def sample_wandb_hyperparams(params, int_hparams=None):
             if v['distribution'] in {'uniform', 'q_uniform'} or v['distribution'] in {'q_uniform_values', 'uniform_values'}:
                 val = random.uniform(v['min'], v['max'])
                 if v['distribution'].startswith("q_"):
-                    sampled[k] = int(val)
+                    val = int(val)
+                sampled[k] = val
             elif v['distribution'] == 'normal':
                 sampled[k] = random.normalvariate(v['mean'], v['std'])
             elif v['distribution'] == 'log_uniform_values':
@@ -246,6 +247,8 @@ def sample_wandb_hyperparams(params, int_hparams=None):
             raise NotImplementedError # f"Value {v} not recognized."
         if k in int_hparams:
             sampled[k] = int(sampled[k])
+
+        assert k in sampled, f"Hparam {k} not successfully sampled."
     return sampled
 
 
