2fde88690b9b66856b147fb9e1e996ca1b84e2e8
vlad <vlmr.mko@gmail.com>
2024-05-14 00:00:45 -0700

    added trajectory rendering options to model trajectory visualizer

--- experiments/show_model_trajectory.py
+++ experiments/show_model_trajectory.py
@@ -12,7 +12,7 @@ from arDDPG import arDDPG
 
 from gymnasium.wrappers import RecordVideo
 
-def main(path):
+def main(path=None, video=False, n_steps=1000, n_episodes=1):
     if '/' in path:
         path_strs = path.split('/')
         model_name = path_strs[-1]
@@ -21,20 +21,24 @@ def main(path):
     model_cls_name, env_id = model_name.split('_')
     model_cls = globals()[model_cls_name]
     # env = gym.make(env_id)
-    model = model_cls.load(path, render=True, save_best=False, render_mode='rgb_array')
+    model = model_cls.load(path, render=True, save_best=False, render_mode='rgb_array' if video else 'human')
     # model.eval_env.render_mode = 'rgb_array'
-    model.eval_env = RecordVideo(
-        model.eval_env,
-        video_folder='best_models/',
-        episode_trigger=lambda episode_id: episode_id % 1 == 0,
-        video_length=10000
-    )
-    model.evaluate(10, render=True)
+    if video:
+        model.eval_env = RecordVideo(
+            model.eval_env,
+            video_folder='best_models/',
+            episode_trigger=lambda episode_id: episode_id % 1 == 0,
+            video_length=n_steps
+        )
+    model.evaluate(n_episodes, render=True)
     model.eval_env.close()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', default='best_models/ASQL_PongNoFrameskip-v4', type=str)
+    parser.add_argument('--video', default=False, type=bool)
+    parser.add_argument('--n_steps', default=10000, type=int)
+    parser.add_argument('--n_episodes', default=10, type=int)
     args = parser.parse_args()
-    main(args.path)
+    main(**vars(args))
